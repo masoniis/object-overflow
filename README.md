b6fcc86cf3cf72611728d492ea431ce63a8d4d24
CNSchaller <calvinnoahschaller@gmail.com>
2025-11-27 06:57:40 +0000

    project init

---
+++ README.md
@@ -0,0 +1,14 @@
+# Object overflow
+
+Incremental object game
+
+## Running project
+
+Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
+
+```sh
+npm run dev
+
+# or start the server and open the app in a new browser tab
+npm run dev -- --open
+```
---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+    systems.url = "github:nix-systems/default";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+      treefmt-nix,
+      systems,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            bun
+            just
+          ];
+        };
+      }
+    )
+    // (
+      let
+        # Small tool to iterate over each systems
+        eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+
+        # Eval the treefmt modules from ./treefmt.nix
+        treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
+      in
+      {
+        # for `nix fmt`
+        formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
+        # for `nix flake check`
+        checks = eachSystem (pkgs: {
+          formatting = treefmtEval.${pkgs.system}.config.build.check self;
+        });
+      }
+    );
+}
